975d55831cb4d20683041ce77cdb86eacf165157
Jan Schulze <58750565+ThatJanGuy@users.noreply.github.com>
2022-10-04 20:57:44 +0200

    Early stages

---
+++ MoneyTracker/Menu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Spectre.Console;
+
+namespace MoneyTracker
+{
+
+    public class Menu
+    {   
+        public Style? MenuStyle { get; set; }
+        public string? Title { get; set; }
+
+
+
+
+        // Ask for the user's favorite fruit
+        var menuChoice = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("Main Menu")
+                .AddChoices(new[] {
+                    "Show transactions",
+                    "Add new transaction",
+                    "Edit transactions",
+                    "Save and quit"
+                }
+                )
+            );
+
+
+    }
+}
---
+++ MoneyTracker/Program.cs
@@ -0,0 +1,9 @@
+﻿//Settings
+using MoneyTracker.Pages;
+using Spectre.Console;
+
+Style MenuStyle = new(Color.Black, Color.Green1);
+
+Home.Show();
+
+Console.ReadLine();
